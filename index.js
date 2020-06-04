fc6054f9e7177359887de34956c960c36544b5f0
sakmobile <sakmobile@users.noreply.github.com>
2020-06-04 17:28:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,37 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
 app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.text
-  var sender = req.body.events[0].source.userId
-  var replyToken = req.body.events[0].replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
-  // console.log(req.body.events[0])
-  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
-  }
-  res.sendStatus(200)
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
 })
-
-function sendText (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer F4ptH38wFe7cYDr7B+0L+GxHxFDEixnLmeNIF5d+NYzh2ne0QgFNTNeceqyz95dwe+TW7DaVgb/qWr4RC9+M+xLFgCGx/BRF8UVaaHU1T1zIv/OqFUWytzW3mzZGbh0MiBqcLb0IJMqj30vQHWt7fgdB04t89/1O/w1cDnyilFU='
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {F4ptH38wFe7cYDr7B+0L+GxHxFDEixnLmeNIF5d+NYzh2ne0QgFNTNeceqyz95dwe+TW7DaVgb/qWr4RC9+M+xLFgCGx/BRF8UVaaHU1T1zIv/OqFUWytzW3mzZGbh0MiBqcLb0IJMqj30vQHWt7fgdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
 }
-
-app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
-})
